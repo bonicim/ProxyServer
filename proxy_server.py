3b10a262dd36e6c80b821b5d414cabc590c14ed4
Mark Bonicillo <markabonicillo@gmail.com>
2017-10-08 01:43:04 +0100

    Adding function stubs. Need to add comments

---
+++ proxy_server.py
@@ -0,0 +1,73 @@
+import socket
+import _thread
+import time
+import select
+
+
+def main():
+    init_srv_sck()
+
+
+def init_srv_sck():
+    """
+
+    :return:
+    """
+
+
+def start_srv_sck():
+    """
+
+    :return:
+    """
+    return srv_sck
+
+
+def handle_multi_threads(srv_sock):
+    """
+
+    :param srv_sock:
+    :return:
+    """
+
+
+def handler(cli_sck):
+    """
+
+    :param cli_sck:
+    :return:
+    """
+
+
+def parse_req_line(req):
+    """
+
+    :param req:
+    :return:
+    """
+    return (host_name, port)
+
+
+def init_tcp_conn(host_name, port):
+    """
+
+    :param host_name:
+    :param port:
+    :return:
+    """
+    return real_srv_sock
+
+
+def recv_cli_sck_data(cli_sck):
+    """
+
+    :param cli_sck:
+    :return:
+    """
+    return b''
+
+
+if __name__ == "__main":
+    main()
+
+main()
